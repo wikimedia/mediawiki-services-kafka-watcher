ca43e8b0166f3280f311eb602f8780725c7c8b69
Stanislav Malyshev <smalyshev@gmail.com>
2016-04-15 18:11:33 -0700

    Add substitutions

--- handlers/Memcached.py
+++ handlers/Memcached.py
@@ -1,7 +1,6 @@
 import memcache
 
-class Memcached:
-
+class Memcached(object):
     def __init__(self, hostname, **params):
         self.mc = memcache.Client([hostname])
 
@@ -12,8 +11,12 @@ class Memcached:
         getattr(self, cmd)(message)
 
     def set(self, message):
-        print("Set {0}-{1}-{2}".format(message['key'].encode('utf-8'), message['val'].encode('utf-8'), int(message['ttl'])))
-        self.mc.set(message['key'].encode('utf-8'), message['val'].encode('utf-8'), int(message['ttl']) )
+        text = message['val'].encode('utf-8')
+        if message.get('sbt', None):
+            purge_time = time.time() + message.get('uto', 0)
+            text = text.replace('$UNIXTIME$', '%.6f' % purge_time)
+#        print("Set {0}-{1}-{2}".format(message['key'].encode('utf-8'), text, int(message['ttl'])))
+        self.mc.set(message['key'].encode('utf-8'), text, int(message['ttl']) )
 
     def delete(self, message):
         self.mc.delete(message['key'])
