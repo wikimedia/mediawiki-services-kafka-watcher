76800e7ab2a481fb5c04faa8fec8cb239e77fea6
Stanislav Malyshev <smalyshev@gmail.com>
2016-04-22 16:31:27 -0700

    Switch to pymemcache for finer control and add retries on network problems.
    - Add client name and kafka host
    
    Change-Id: I452c2110599faed7fe7d20e2fb5b5c05f023288b

--- handlers/Memcached.py
+++ handlers/Memcached.py
@@ -1,24 +1,46 @@
-import memcache
+from __future__ import print_function
+
+from pymemcache.client.base import Client
+from pymemcache.exceptions import MemcacheUnexpectedCloseError
 import time
 
 
 class Memcached(object):
+    DELAY = 0.5
+    DEBUG = False
 
-    def __init__(self, hostname, **params):
-        self.mc = memcache.Client([hostname])
+    def __init__(self, hostname, port, **params):
+        self.mc = Client((hostname, port))
 
     def handle(self, topic, message):
+        """
+        """
+        if 'cmd' not in message:
+            raise Exception("Bad message: no command")
         cmd = message['cmd']
         if not hasattr(self, cmd):
             raise Exception("Unknown command: " + cmd)
-        getattr(self, cmd)(message)
+        tryit = True
+        while tryit:
+            tryit = False
+            try:
+                getattr(self, cmd)(message)
+            except MemcacheUnexpectedCloseError:
+                # Server dropped dead - we'll retry
+                tryit = True
+            except IOError:
+                # Something network-related - retry
+                tryit = True
+            if tryit:
+                time.sleep(self.DELAY)
 
     def set(self, message):
         text = message['val'].encode('utf-8')
         if message.get('sbt', None):
             purge_time = time.time() + message.get('uto', 0)
             text = text.replace('$UNIXTIME$', '%.6f' % purge_time)
-#        print("Set {0}-{1}-{2}".format(message['key'].encode('utf-8'), text, int(message['ttl'])))
+        if self.DEBUG:
+            print("Set {0}-{1}-{2}".format(message['key'].encode('utf-8'), text, int(message['ttl'])))
         self.mc.set(message['key'].encode('utf-8'), text, int(message['ttl']))
 
     def delete(self, message):
--- kafka-watcher.py
+++ kafka-watcher.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
+from __future__ import print_function
+
+from kafka import KafkaConsumer
 import yaml
 import argparse
-from kafka import KafkaConsumer
 import imp
 import json
 import sys
+import socket
+import hashlib
+
+# Kafka client is https://github.com/dpkp/kafka-python
 
 parser = argparse.ArgumentParser(description='Process cache relay commands from Kafka')
 parser.add_argument('--config', required=True, help='YAML configuration file')
@@ -25,20 +31,29 @@ for listener in config['listeners']:
     klass = getattr(mod, listener['handler'])
     handlers[listener['topic']] = klass(**listener['params'])
 
-consumer = KafkaConsumer(*topics)
+if 'server' in config:
+    server = config['server']
+else:
+    server = 'localhost'
+
+client_id = "kafka-watcher-%s-%s" % (socket.gethostname(), hashlib.md5(script_args.config).hexdigest())
+
+print("Connecting to %s as %s" % (server, client_id))
+
+consumer = KafkaConsumer(*topics, bootstrap_servers=server, client_id=client_id)
 for msg in consumer:
     if msg.topic not in handlers:
-        print("Weird, unknown topic %s" % msg.topic)
+        print("Weird, unknown topic %s" % msg.topic, file=sys.stderr)
         continue
     try:
         data = json.loads(msg.value)
     except ValueError:
         data = None
     if not data:
-        print("Could not parse data, meh")
+        print("Could not parse data, meh", file=sys.stderr)
         continue
     try:
         handlers[msg.topic].handle(msg.topic, data)
     except:
         e = sys.exc_info()
-        print("Oops, something happened: " + str(e))
+        print("Oops, something happened: " + str(e), file=sys.stderr)
